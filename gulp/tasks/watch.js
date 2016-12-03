d7002086cdc1daece180c747f32b309655241d04
Chris <jcj4241@gmail.com>
2016-12-03 17:48:50 -0500

    Completed work on the Header.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,21 +4,25 @@ browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
 
-  browserSync.init({
-    notify: true,
-    server: {
-      baseDir: "app"
-    }
-  });
+    browserSync.init({
+      notify: true,
+      server: {
+        baseDir: "app"
+      }
+    });
 
-  watch('./app/index.html', function() {
-    browserSync.reload();
-  });
+    watch('./app/index.html', function() {
+      browserSync.reload();
+    });
 
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('cssInject');
-  });
+    watch('./app/assets/styles/**/*.css', function() {
+      gulp.start('cssInject');
+    });
 
+    watch('./app/assets/scripts/**/*.js', function() {
+      gulp.start('scriptsRefresh');
+
+    });
 
   });
 
@@ -26,3 +30,7 @@ gulp.task('watch', function() {
     return gulp.src('./app/temp/styles/styles.css')
       .pipe(browserSync.stream());
   }) ;
+
+  gulp.task('scriptsRefresh', ['scripts'], function() {
+    browserSync.reload();
+  });
